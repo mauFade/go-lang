d265d443386c08a014eb2f10789a099bdab1ac02
Mauricio <mauricio.cds00@gmail.com>
2023-01-31 17:51:20 -0300

    feat: sum operations

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+)
 
 func main() {
-	http.ListenAndServe(":9091", nil)
+	fmt.Println(sum(5,5))
+}
+
+func sum (numA, numB int) (int, error) {
+	if numA + numB > 10 {
+		return 0, fmt.Errorf("This sum is greater than 10.")
+	}
+	return numA + numB, nil
 }
